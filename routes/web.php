4e5ea43003e65662032b9f3825a7bcba9e71abbb
Rajender Kumar <rajendrak@beckett.com>
2024-10-09 21:14:51 +0530

    test changes

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
-
+// test
 Route::get('/', [HomeController::class, 'index']);
 Route::get('about', [HomeController::class, 'about']);
 Route::get('services', [HomeController::class, 'services']);
