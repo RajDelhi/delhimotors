c6ab20fb879e3f0b099f997d25b22c096448bdf7
Rajender Kumar <rajendrak@beckett.com>
2024-10-10 12:06:21 +0530

    test changes

--- resources/views/delhi-motors/contact.blade.php
+++ resources/views/delhi-motors/contact.blade.php
@@ -56,8 +56,10 @@
                     </div>
                 </div>
                 <div class="col-md-6 wow fadeIn" data-wow-delay="0.1s">
+
+
                     <iframe class="position-relative rounded w-100 h-100"
-                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3001156.4288297426!2d-78.01371936852176!3d42.72876761954724!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4ccc4bf0f123a5a9%3A0xddcfc6c1de189567!2sNew%20York%2C%20USA!5e0!3m2!1sen!2sbd!4v1603794290143!5m2!1sen!2sbd"
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d23118.007920937973!2d77.27701738827288!3d28.508185646871375!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390ce70043f42f83%3A0xd0b68a9f07f15e95!2sDelhi%20Motors%20-%20Bosch%20Car%20service%20center!5e1!3m2!1sen!2sin!4v1728541995788!5m2!1sen!2sin"
                         frameborder="0" style="min-height: 350px; border:0;" allowfullscreen="" aria-hidden="false"
                         tabindex="0"></iframe>
                 </div>
