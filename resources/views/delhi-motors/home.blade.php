87e969c0329d2ffc1693c612462c48ef883bc2e0
Rajender Kumar <rajendrak@beckett.com>
2024-10-19 12:06:22 +0530

    test

--- resources/views/delhi-motors/home.blade.php
+++ resources/views/delhi-motors/home.blade.php
@@ -6,7 +6,7 @@
     <div class="container-fluid p-0 mb-5">
         <div id="header-carousel" class="carousel slide" data-bs-ride="carousel">
             <div class="carousel-inner">
-                <div class="carousel-item active">
+                <!-- <div class="carousel-item active">
                     <img class="w-100" src="img/carousel-bg-1.jpg" alt="Image">
                     <div class="carousel-caption d-flex align-items-center">
                         <div class="container">
@@ -25,8 +25,8 @@
                             </div>
                         </div>
                     </div>
-                </div>
-                <div class="carousel-item">
+                </div> -->
+                <div class="carousel-item active">
                     <img class="w-100" src="img/carousel-bg-2.jpg" alt="Image">
                     <div class="carousel-caption d-flex align-items-center">
                         <div class="container">
