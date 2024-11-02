a0d2b01b5ecfee75aa69863a55068fcd4c1e3888
Rajender Kumar <rajendrak@beckett.com>
2024-11-02 15:41:21 +0530

    test

--- resources/views/delhi-motors/home.blade.php
+++ resources/views/delhi-motors/home.blade.php
@@ -242,7 +242,7 @@
                 <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
                     <div class="team-item">
                         <div class="position-relative overflow-hidden">
-                            <img class="img-fluid" src="img/brand-5.png" alt="">
+                            <img class="img-fluid" src="img/brand-9.png" alt="">
                         </div>
                     </div>
                 </div>
