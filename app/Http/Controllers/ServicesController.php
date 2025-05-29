b7bfb29ef6b93ac54dcb9a565045a8081fa5e2cd
deepwebphp <56186889+deepwebphp@users.noreply.github.com>
2025-05-29 16:47:38 +0530

    change meta for audi

--- app/Http/Controllers/ServicesController.php
+++ app/Http/Controllers/ServicesController.php
@@ -8,19 +8,19 @@ class ServicesController extends Controller
 {
     public function audi_service_center_in_delhi()
     {
-        $data['page_title'] = "Audi Repair and Audi Service Center in Delhi Near Me";
-        $data['description'] = "Audi repairs, Audi Service Center in Delhi Near Me ensure that your vehicle receives top-quality maintenance and care, When serviced by the skilled team at Delhi Motors.";
-        $data['p_keyword'] = "Audi Service Center in Delhi Near Me";
-        $data['s_keyword'] = "Audi repair, Audi Service Center in Delhi, Audi Service Center in Delhi Near Me, Car Service Center in Delhi NCR";
+        $data['page_title'] = "Discover Premium Audi Repair Solutions in Mohan estateDelhi NCR";
+        $data['description'] = "Keep your Audi in top shape with Delhi Motors India. Our expert technicians in Delhi specialize in luxury vehicle maintenance and repairs, ensuring peak performance.";
+        $data['p_keyword'] = "audi car service centre in Mohan estate Delhi";
+        $data['s_keyword'] = "Luxury Car Service Centre in Mohan estate Delhi NCR, best car repair in Mohan estate, Popular Car Repair & Services in Mohan corporative, car service centre in Badarpur, top car repair service in Mohan estate Delhi, top rated car service and repair in Mohan estate Delhi NCR, vehicle repair services in Badarpur delhi, luxury car repair shop in Badarpur delhi ncr";
         return view('pages/audi', $data);
     }
 
     public function bmw_sevice_center_in_delhi()
     {
-        $data['page_title'] = "BMW Repair, BMW Service Center Near Me in Delhi";
+        $data['page_title'] = "Experience Premium BMW Care at Delhi Motors India";
         $data['description'] = "Delhi Motors India provide best BMW service and BMW service center Near me in delhi";
-        $data['p_keyword'] = "BMW Service Center near me In Delhi NCR";
-        $data['s_keyword'] = "BMW repair, BMW service center near me in delhi, Best BMW repair center in delhi, car Service Center in delhi near me";
+        $data['p_keyword'] = "BMW car service centre in Mohan estate Delhi";
+        $data['s_keyword'] = "Luxury Car Service Centre in Mohan estate Delhi NCR, best car repair in Mohan estate, Popular Car Repair & Services in Mohan  corporative, car service centre in Badarpur, top car repair service in Mohan estate Delhi, top rated car service and repair in Mohan estate Delhi NCR, vehicle repair services in Badarpur delhi, luxury car repair shop in Badarpur delhi ncr";
         return view('pages/bmw', $data);
     }
 
