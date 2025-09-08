f697041226977b7a39cc867d318697406da011c2
deepwebphp <56186889+deepwebphp@users.noreply.github.com>
2025-09-08 23:23:44 +0530

    title change

--- resources/views/delhi-motors/blog7.blade.php
+++ resources/views/delhi-motors/blog7.blade.php
@@ -19,7 +19,7 @@
     <div class="container-fluid page-header mb-5 p-0" style="background-image: url(img/carousel-bg-1.jpg);">
         <div class="container-fluid page-header-inner py-5">
             <div class="container text-center">
-                <h1 class="display-3 text-white mb-3 animated slideInDown">Car service and repair in Delhi NCR</h1>
+                <h1 class="display-3 text-white mb-3 animated slideInDown">Mechanic on Wheels Delhi NCR</h1>
                 <!-- <nav aria-label="breadcrumb">
                     <ol class="breadcrumb justify-content-center text-uppercase">
                         <li class="breadcrumb-item"><a href="#">Home</a></li>
